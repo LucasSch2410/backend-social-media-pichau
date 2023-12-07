c234b0bf8a54ef63f14597e3567fa0f7c775d95a
Lucas <lucasschroeder2410@gmail.com>
2023-12-07 16:55:17 -0300

    Finish the create_text function and docstrings

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ from app.resource_handler import load_background, load_product_image, load_font
 
 
 def main(): 
-    dbx = authenticate_dropbox()
+    dbx = dropbox_connect()
     
     product_name = input("Nome do produto: ").upper().split(", ")
     
     for i in range(len(product_name)):
         product_name[i].replace(",", "")
-
-
-    print(product_name)
+    
+    
     # Import essential files
     template_storie = load_background("data/input/templates/template-storie.jpg")
     
@@ -30,10 +29,10 @@ def main():
 
 
     # Put the text into template
-    height = create_text(product_name, template_storie, load_font())
+    text_height = create_text(product_name, template_storie, load_font())
 
     # Put the image into template
-    product_create(image, template_storie, height)
+    product_create(image, template_storie, text_height)
 
     template_storie.save("data/output/file.png")
 
@@ -45,32 +44,39 @@ def dropbox_connect():
     """Create a connection to Dropbox."""
 
     try:
-        dbx = dropbox.Dropbox('sl.BrIZf3B-rqu6qAAJ5D3IdpaKA-AnMJPbRCY3qRlLdAFi7_CaLuuTBFHlyMA0wkry_h1KSi9vvmqRssbH7XOwPFyYOiV0Y1ZV1ZEFfhhOGRpi_MCypU9eFRtFXXlnetRIJtAUHBFN5Shl8z-isnYOBWM')
+        dbx = dropbox.Dropbox('sl.BrRAK8HuXD67WPeyGj2zmEpjRrgYEUwuRvSWXR29rOkwWLd7ihrXYN_9059SIjLqEjMRdhUtK74YFYk-J-1i6xOA0MGO4oKZaMG5s3tXGcbKHCCVWdDtzi3LIiePGw5b3h40byWd3CTKXlCaqt6fjjA')
     except AuthError as e:
         print('Erro ao se conectar ao Dropbox com o token de acesso: ' + str(e))
         sys.exit(3)
     return dbx
 
 
-# Resize the Image and put in the template
-def product_create(product, template, height):
+def product_create(product, template, text_height):
+    """ 
+    Resize the Image and put in the template.
+    
+    Args:
+        product: The product image object. (Image)
+        template: The template image object. (Image)
+        text_height: The height of the text created. (int)
+    """
+
     try:
         # Calculate the width and height of the non-transparent region
-        bbox = product.getbbox()
-        croped_image = product.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
+        cropped_image = product.crop(product.getbbox())
         
         # Calculate the aspect ratio
-        aspect_ratio = croped_image.width / croped_image.height
+        aspect_ratio = cropped_image.width / cropped_image.height
 
         # Calculate the new height based on the template aspect ratio
         new_width = template.width - 200
         new_height = int(new_width / aspect_ratio)
 
         # Resize the product image with the new dimensions
-        new_image = croped_image.resize((new_width, new_height))
+        resized_image = cropped_image.resize((new_width, new_height))
 
-        position = (100, 480 + height) 
-        template.paste(new_image, position, mask=new_image) 
+        position = (100, 480 + text_height) 
+        template.paste(resized_image, position, mask=resized_image) 
         return True
     except Exception as e:
         print("Error when add the product image into background: ", e)
@@ -82,8 +88,20 @@ def create_layout():
     return None
 
 
-# Wrap the text if it's bigger than the template width
 def text_wrap(text, font, max_width):
+    """ 
+    Wrap the text if it's bigger than the template width.
+    
+    Args:
+        text: The full text separated by commas. (str)
+        font: The font object. (Font)
+        max_width: The maximum width that the text will can be. (Int)
+    
+    Return:
+        A lines list with length less than or equal to 3, each item represents 
+        a line to draw. (List[str])
+    """
+
     lines = []
     # Return if the text width is smaller than max_width
     if font.getlength(text) <= max_width:
@@ -97,25 +115,40 @@ def text_wrap(text, font, max_width):
                 line = line + words[i] + " "
                 i += 1
             lines.append(line)  
-            # Stop if have too many lines and excludes the last two characters
+            # Stop if there more than three lines and removes the last two characters
             if len(lines) > 2:
-                lines[len(lines)-1] = lines[len(lines)-1][:-2]
+                if lines[-1][-2:] == ", ":
+                    lines[-1] = lines[-1][:-2]
                 break  
     return lines
 
 
-# Creates the top text in the image
+
 def create_text(text, template, font):
+    """
+    Creates the text on the top of the image.
+
+    Args:
+        text: A list of strings representing the text to be displayed. (List[str])
+        template: The template image object. (Image)
+        font: The font object. (Font)
+    
+    Return:
+       The height of the text created. (int)
+    """
+
     draw = ImageDraw.Draw(template)
 
-    full_text = ', '.join(text[:len(text)-1])
-    wraped_text = text_wrap(full_text, font, template.width - 200)
-    multilines_text = '\n'.join(wraped_text)
-    draw.text((110,480), multilines_text, font=font, fill=(255, 255, 255), stroke_width=10, stroke_fill=(0, 0, 0))
+    full_text = ', '.join(text[:len(text)-1]) # Combine all text except the last element (SKU).
+    wraped_text = text_wrap(full_text, font, template.width - 200) # Wrap text based on template width.
+    multilines_text = '\n'.join(wraped_text) # Join wrapped lines with newline characters.
+
+    draw.text((110,480), multilines_text, font=font, fill=(255, 255, 255), # Draw text with white fill.
+              stroke_width=10, stroke_fill=(0, 0, 0)) # Add black stroke around text for better visibility.
     
-    # Returns the height of the text
-    height = font.getbbox(multilines_text)[3] * len(wraped_text) + (25 * len(wraped_text))
-    return height
+    # Calculate text height based on font size and number of lines.
+    text_height = font.getbbox(multilines_text)[3] * len(wraped_text) + (25 * len(wraped_text))
+    return text_height
 
 
 
