9e5d415440425b2699f5a637293d0404dc3a2f91
Lucas <lucasschroeder2410@gmail.com>
2024-01-01 15:05:32 -0300

    Methods changes in routes

--- app/media.py
+++ app/media.py
@@ -1,8 +1,10 @@
 import csv, requests
 import dropbox
+import base64
+from io import BytesIO
 
 from PIL import Image, ImageDraw, ImageFont
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, jsonify, request, redirect, send_file
 from bs4 import BeautifulSoup
 from dropbox.exceptions import AuthError
 
@@ -12,16 +14,22 @@ from app.layouts.stories.stories_layout import InstagramLayout
 app = Flask(__name__)
 
 
-@app.route('/instagram', methods=['POST'])
+@app.route('/instagram', methods=['GET'])
 def create_media():
     try:
         dbx = dropbox_connect()
 
-        final_layout_image = InstagramLayout(request.json["product"], dbx)()
+        final_layout_image = InstagramLayout(request.args["product"], dbx)()
 
         if final_layout_image:
             final_layout_image.save("data/output/file.png")  # Save the image if successful
-            return 'Success!'
+            with open("data/output/file.png", 'rb') as f:
+                image_base64 = base64.b64encode(f.read())
+                image_base64_str = image_base64.decode('utf-8')
+            response = jsonify(image_base64_str)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+
         else:
             raise Exception("Error occurred during layout creation.")
     except AuthError as e:
@@ -30,16 +38,17 @@ def create_media():
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
 
 
-@app.route('/sheet', methods=['POST'])
+@app.route('/sheet', methods=['GET'])
 def scrap_sheet():
     """
     Scrap the HTML of the Google Sheet in the URL from the request
     """
 
-    html = requests.get(request.json["sheet_url"]).text
-    soup = BeautifulSoup(html, "html.parser")
+    html = requests.get(request.args['sheet_url'])
+    soup = BeautifulSoup(html.text, "html.parser")
     table = soup.find("tbody")
-    products = []
+    products = {}
+    index = 0
 
     # Iterate across product lines
     for line in table:
@@ -47,27 +56,33 @@ def scrap_sheet():
         if index > 1:
             product = line.find('td').get_text()
             if product != "":
-                products.append(product)
-    return jsonify({"products": products})
+                products.update({index:product})
+                index += 1
+
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 # Auth2 redirect
 @app.route('/auth2', methods=['GET'])
 def auth2():
-    authorization_code = str(request.args.get('code'))
+    client_id = 'emm8mikk0bmdwbn'
+    client_secret = 'ldxj4yfnixjfhdz'
+    refresh_token = '3_63z0G-Y_IAAAAAAAAAAcgOLuMn6bvTvJDW4fYcu9FfsEpdm-YfdP96FWilulA9'
+
     token_url = "https://api.dropboxapi.com/oauth2/token"
     params = {
-        "code": authorization_code,
-        "grant_type": 'authorization_code',
-        "redirect_uri": 'http://localhost:5000/auth2',
-        "client_id": 'emm8mikk0bmdwbn',
-        "client_secret": 'ldxj4yfnixjfhdz'
+        "grant_type": 'refresh_token',
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "client_secret": client_secret
     }
 
     r = requests.post(token_url, data=params)
 
     if r.status_code != 200:
-        return f'Error {r}'
+        return f'Error {r.content}'
     
     access_token = r.json()['access_token']
     
@@ -79,9 +94,9 @@ def dropbox_connect():
     """Create a connection to Dropbox."""
 
     try:
-        dbx = dropbox.Dropbox('sl.Bsl4y581fMelsbwTc4XMn56KesbVjWX51-LyZ0DtHkPmtF54DkmWcFhT7oQPMyKjr97typUSgdw7SQg5aImwYkCZg0icWNZChh_-n0o7ysDW2Pz1w0RI9WyDobvMSF7lv-Xga25IfcJldr0qBe1jEZ8')
+        dbx = dropbox.Dropbox('sl.BsxYynhTTiphy9usSzmW0gEhcehSH1Hz0gir5yl5ZIa4qwDp_ahg35OjSa5rKcCRJ1_N0TbKR1OMujy3Bu_OnINFpgisznzOS78FA8NQdvVyEyl-Wg10n2gx2prlgUGzn0ahqPqPTQ68Xi4OXyZbxwc')
     except AuthError as e:
         raise Exception(f'Error connecting to Dropbox with access token: {str(e)}')
     return dbx
 
-app.run(port=5000, host='localhost', debug=True)
+app.run(port=3000, host='localhost', debug=True)
